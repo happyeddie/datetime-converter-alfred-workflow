ad4f4a66f14dbabfa67e6ef9f7786ba6ab207421
happyeddie <happyeddie@gmail.com>
2022-04-19 11:34:26 +0800

    add multi timezone support

--- index.php
+++ index.php
@@ -2,48 +2,71 @@
 $query  = $argv[1];
 $result = "";
 
+$tzs = explode(",", getenv("TZ"));
+$default_tz = date_default_timezone_get();
+
 // timestamp
 $rules_timestamp                = "/^\d+$/";
-$rules_timestamp_second         = "/^\d{10}$/";
+$rules_timestamp_second         = "/^\d{1,10}$/";
 $rules_timestamp_millisecond    = "/^\d{13}$/";
 
-if (preg_match($rules_timestamp, $query)) {
-    if (preg_match($rules_timestamp_second, $query)) {
-        $result = date("Y-m-d H:i:s", $query);
-    }
-    else if (preg_match($rules_timestamp_millisecond, $query)) {
-        $second = substr($query, 0, 10);
-        $ms     = substr($query, 10, 3);
-        $result = date("Y-m-d H:i:s", $second) . '.' . $ms;
-    }
-    else {
-        $result = "{$query} is invaild timestamp";
+foreach ($tzs as $key => $tz) {
+    $tzs[$key] = trim($tz);
+
+    if ($tzs[$key] == "") {
+        unset($tzs[$key]);
     }
 }
-else {
-    $split  = explode(".", $query);
-    $result = strtotime($split[0]);
 
-    if (count($split) > 1 && preg_match("/^\d{3}$/", $split[1])) {
-        $result .= $split[1];
-    }
+if (!$tzs) {
+    $tzs = [
+        $default_tz,
+    ];
+}
 
-    if (!preg_match($rules_timestamp, $result)) {
-        $result = "{$query} is invaild datetime";
+$items  = [];
+
+foreach ($tzs as $tz) {
+    date_default_timezone_set($tz);
+
+    if (preg_match($rules_timestamp, $query)) {
+        if (preg_match($rules_timestamp_second, $query)) {
+            $result = date("Y-m-d H:i:s", $query);
+        }
+        else if (preg_match($rules_timestamp_millisecond, $query)) {
+            $second = substr($query, 0, 10);
+            $ms     = substr($query, 10, 3);
+            $result = date("Y-m-d H:i:s", $second) . '.' . $ms;
+        }
+        else {
+            $result = "{$query} is invaild timestamp";
+        }
     }
+    else {
+        $split  = explode(".", $query);
+        $result = strtotime($split[0]);
+    
+        if (count($split) > 1 && preg_match("/^\d{3}$/", $split[1])) {
+            $result .= $split[1];
+        }
+    
+        if (!preg_match($rules_timestamp, $result)) {
+            $result = "{$query} is invaild datetime";
+        }
+    }
+    
+    $items[] = [
+        "title" => "{$result}",
+        "subtitle" => "{$tz}",
+        "arg"   => "{$result}",
+    ];
 }
 
 $output = [
-    "items" => [
-        [
-            "title" => "{$result}",
-            "arg"   => "{$result}",
-            "icon"  => [
-                "path"  => "./icon.png",
-            ],
-        ]
-    ]
+    "items" => $items,
 ];
 
 echo json_encode($output);
+
+date_default_timezone_set($default_tz);
 ?>
